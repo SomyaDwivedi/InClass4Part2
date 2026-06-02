d2b904cb752bbfe67facffb0460c030b4843038e
Somya Dwivedi <somya.dwivedi@yahoo.com>
2026-06-01 21:21:10 -0400

    Added addition,subtraction and multiplication methods

---
+++ InClassPart2/src/main/java/com/mycompany/inclasspart2/InClassPart2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inclasspart2;
+
+/**
+ *
+ * @author Somya Dwivedi, 2026
+ */
+public class InClassPart2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int multiply= num1 * num2;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Multiplicaton = " + multiply);
+    }
+}
